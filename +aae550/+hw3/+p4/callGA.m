0b4a7493bee3f85d56032ef03f3f509a2f5bb057
tsatter <tasat@me.com>
2017-11-13 13:42:41 -0500

    Updated to make faster

--- +aae550/+hw3/+p4/callGA.m
+++ +aae550/+hw3/+p4/callGA.m
@@ -1,31 +1,35 @@
-% this file provides input variables to the genetic algorithm
-% upper and lower bounds, and number of bits chosen for "egg-crate" problem
-% Modified on 11/10/09 by Bill Crossley.
-
 close all;
-clear all;
+clear;
 
 options = aae550.hw3.p3.goptions([]);
 
-vlb = [1 1e-4 1 1e-4 1 1e-4];	%Lower bound of each gene - all variables
-vub = [4 1e-3 4 1e-3 4 1e-3];	%Upper bound of each gene - all variables
-bits =[2 40 2 40 2 40];	%number of bits describing each gene - all variables
+% Gene encoding:
+%   
+%     |   Beam 1     |    Beam 2     |    Beam 3     |
+% x = [Material, Area, Material, Area, Material, Area]
+% Area in [m^2]
+vlb = [1 1e-4 1 1e-4 1 1e-4];	%Lower bound of each gene
+vub = [4 1e-3 4 1e-3 4 1e-3];	%Upper bound of each gene
+bits =[2 30 2 30 2 30];	% Number of bits describing each gene
 
-l = sum(bits);
+l = sum(bits); % Chromosome length
 
-nPop = 4 * l; % Basic guidline
+% Basic guidlines for population and mutation rate
+nPop = 4 * l;
 pMutation = (l + 1) / (2 * nPop * l);
 
+% Set options
 options(11) = nPop; % Set the population size
 options(13) = pMutation; % Set the mutation probability
-options(14) = 1e6;
-%	OPTIONS(11)-Population size (fixed)
-%	OPTIONS(12)-Probability of crossover
-%	OPTIONS(13)-Probability of mutation
-%	OPTIONS(14)-Maximum number of generations, always used as safeguard
+options(14) = 1e6; % Maximum number of generations
+
+% Evaluate
 pMult = 5e2; % Penalty multiplier
 [x,fbest,stats,nfit,fgen,lgen,lfit]= aae550.hw3.p3.GA550(@(x) aae550.hw3.p4.evalTruss(x, pMult),[ ],options,vlb,vub,bits);
 
+% Print results
+resolution = (vub(2) - vlb(2)) / (2^bits(2));
+fprintf('Cross sectional area resolution: %0.9f %sm^2 \n', resolution * 1e12, char(956));
 
 disp(x)
 [phi, g] = aae550.hw3.p4.evalTruss(x)
